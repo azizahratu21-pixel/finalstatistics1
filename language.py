a388c862b8b0cfd2e73e28d4aa761769c6b39ff3
azizahratu21-pixel <azizahratu21@gmail.com>
2025-12-17 13:01:33 +0700

    Add language support for Indonesian and English

---
+++ language.py
@@ -0,0 +1,12 @@
+LANG = {
+    "id": {
+        "title": "Aplikasi Analisis Data",
+        "welcome": "Selamat datang di aplikasi",
+        "description": "Aplikasi ini digunakan untuk analisis data."
+    },
+    "en": {
+        "title": "Data Analysis Application",
+        "welcome": "Welcome to the application",
+        "description": "This application is used for data analysis."
+    }
+}
